4931bd6bbf6ded4bd381cd5824ef4e886fbf8148
Jonathan Wang <31040440+jonathanpwang@users.noreply.github.com>
2024-07-19 17:19:02 -0700

    chore: add "halo2-scroll" feature
    Sync with https://github.com/axiom-crypto/halo2-lib/commit/8a6739c7537ae02f34cb23294f56d6c1cc7b77d1
    NOTE: halo2-scroll uses mv-lookup (logup), which snark-verifier does not
    support, so the "halo2-scroll" currently does not work for anything
    except basic types support.

--- snark-verifier/src/system.rs
+++ snark-verifier/src/system.rs
@@ -1,3 +1,5 @@
 //! Proof systems `snark-verifier` supports
 
+#[cfg(not(feature = "halo2-scroll"))]
+// halo2-scroll uses mv-lookup and the system has not been updated to support it
 pub mod halo2;
